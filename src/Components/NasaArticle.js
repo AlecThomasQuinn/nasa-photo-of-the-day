7156d43e1789801eb1b5888ea30134d95187dcfa
unknown <alec.thomas.quinn@gmail.com>
2019-08-14 15:32:05 -0500

    image rendering onto app from Nasa API

--- src/Components/NasaArticle.js
+++ src/Components/NasaArticle.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 export default function NasaArticle(){
-    //image
+    const [imageURL, setImageURL] = useState('');
     //title
     //date
     //article
@@ -11,13 +11,19 @@ export default function NasaArticle(){
         axios
             .get('https://api.nasa.gov/planetary/apod?api_key=k9ZpiCFaAhQPdf9BdgFkumzsAKCp4bh6maFLZEaf&date=2013-03-14')
             .then(response => {
-                console.log(response.data);
+                console.log('api getted from NasaArticle:', response.data);
+
+                const imgURL = response.data.url;
+                setImageURL(imgURL);
+
+                // can I do this without making the variable?
+
             })
-    }, []); 
+    }, []);  // the empty array is to stop an unintentional 'denial-of-service' attack
 
     return(
         <article>
-            <img />
+            <img src={imageURL}/>
             <h1>Title goes here</h1>
             <h4>Date goes here</h4>
             <p>Article data goes here</p>
